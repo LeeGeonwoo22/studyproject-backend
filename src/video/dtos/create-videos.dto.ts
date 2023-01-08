b2865c37aee8d00f9e7636faff4abb6f35dcf45d
Geon Woo <kwoo.chris@gmail.com>
2023-01-08 23:58:39 +0900

    #1 dto validatior

--- src/video/dtos/create-videos.dto.ts
+++ src/video/dtos/create-videos.dto.ts
@@ -1,13 +1,24 @@
 import { ArgsType, Field} from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 
 @ArgsType()
 export class createVideosDto {
     @Field(type => String)
+    @IsString()
+    @Length(5, 10)
      name : string;
+
     @Field(type => Boolean)
+    @IsBoolean()
      isHeros : boolean;
-    @Field(type => String)
+    
+     @Field(type => String)
+     @IsString()
+     @Length(5, 10)
      genre : string;
+
     @Field(type => String)
+    @IsString()
+    @Length(5, 10)
      heroName : string;
 }
